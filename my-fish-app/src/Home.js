8ff6165b23b4eca8625afbbeec5cd7af64a87eff
Dalton Knuckles <djck244@gmail.com>
2023-06-29 11:24:16 -0700

    updates Home with fake login

--- my-fish-app/src/Home.js
+++ my-fish-app/src/Home.js
@@ -1,10 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 
 
 function Home() {
+    const [showLogin, setShowLogin] = useState(false);
     return (
         <div>
             <h1>Knuckles's Fish Gallery</h1>
+            <button onClick={() => setShowLogin(true)}>Login</button>
+
+      {showLogin && (
+        <div className="login-popup">
+          <div className="login-content">
+            <button onClick={() => setShowLogin(false)}>Close</button>
+            <Login />
+          </div>
+        </div>
+      )}
         </div>
     )
 };
