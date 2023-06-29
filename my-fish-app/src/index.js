506de39e356a94316dbd1fbc1e91c4141ab9e5b3
Dalton Knuckles <djck244@gmail.com>
2023-06-29 11:47:39 -0700

    update to index.js

--- my-fish-app/src/index.js
+++ my-fish-app/src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import { BrowserRouter, Route } from "react-router-dom";
 import './index.css';
 import App from './App';
