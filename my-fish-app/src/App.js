e5bb0a3e1eecf70754ee4a00cc5cac4486ee925a
Dalton Knuckles <djck244@gmail.com>
2023-06-29 10:22:33 -0700

    imports components into app component

--- my-fish-app/src/App.js
+++ my-fish-app/src/App.js
@@ -1,3 +1,8 @@
+import React, { useState, useEffect } from 'react';
+import FishGallery from './FishGallery';
+import Home from './Home';
+import NavBar from './NavBar';
+import TankCalculator from './TankCalculator';
 import logo from './logo.svg';
 import './App.css';
 
@@ -6,17 +11,6 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
