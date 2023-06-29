3379d85bcc9872b172062af4a5c5a77bd315e862
Dalton Knuckles <djck244@gmail.com>
2023-06-29 10:34:31 -0700

    updates Home

--- my-fish-app/src/FishGallery.js
+++ my-fish-app/src/FishGallery.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 
 
 
-function FishGallery () {
+function FishGallery() {
     return (
         <div></div>
     )
--- my-fish-app/src/NavBar.js
+++ my-fish-app/src/NavBar.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 
 
-function NavBar () {
+function NavBar() {
     return (
         <div></div>
     )
