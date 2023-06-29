789cd802f69a4e359323c9fbdc8f6e057873b9d3
Dalton Knuckles <djck244@gmail.com>
2023-06-29 11:10:00 -0700

    updates home with fish gallery

--- my-fish-app/src/App.js
+++ my-fish-app/src/App.js
@@ -3,15 +3,31 @@ import FishGallery from './FishGallery';
 import Home from './Home';
 import NavBar from './NavBar';
 import TankCalculator from './TankCalculator';
+import fishData from './fishData.json';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [fishes, setFishes] = useState(fishData.fish);
+
+  const handleLike = (fishId) => {
+    setFishes((prevFishes) =>
+      prevFishes.map((fish) => {
+        if (fish.id === fishId) {
+          return { ...fish, likes: fish.likes + 1 };
+        }
+        return fish;
+      })
+    );
+  };
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
       </header>
+      <div>
+        <FishGallery fishData={fishes} handleLike={handleLike} />
+      </div>
     </div>
   );
 }
